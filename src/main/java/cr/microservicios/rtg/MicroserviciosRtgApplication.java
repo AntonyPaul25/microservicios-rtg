897735a6417519c1c2c49798b44c40beb022015f
Tonny <antony.vp.25@gmail.com>
2021-08-03 20:22:02 -0500

    primera

---
+++ src/main/java/cr/microservicios/rtg/MicroserviciosRtgApplication.java
@@ -0,0 +1,13 @@
+package cr.microservicios.rtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosRtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosRtgApplication.class, args);
+	}
+
+}
